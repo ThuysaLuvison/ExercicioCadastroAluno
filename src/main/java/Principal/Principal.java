409c880b4761eac88dd705e5efdc1b8f7df7433e
thuys <thuys@10.150.102.145>
2024-04-30 19:15:15 -0300

    add v1 template

---
+++ src/main/java/Principal/Principal.java
@@ -0,0 +1,13 @@
+package Principal;
+
+import View.FrmMenuPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
+
+        objetotela.setVisible(true);
+    }
+}
