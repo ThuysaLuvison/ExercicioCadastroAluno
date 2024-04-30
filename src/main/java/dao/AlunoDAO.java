916855204ddeec283250a5b95c67becd2508b192
1072419878 <1072419878@UNS11DT099950.animaedu.intranet>
2024-04-30 19:56:13 -0300

    AlunoDAO

--- src/main/java/dao/AlunoDAO.java
+++ src/main/java/dao/AlunoDAO.java
@@ -1,8 +1,15 @@
-package dao;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
 
 import java.util.ArrayList;
 import model.Aluno;
 
+/**
+ * Aqui vamos simular a persistência de dados.
+ */
 public class AlunoDAO {
 
     public static ArrayList<Aluno> minhaLista = new ArrayList<>();
