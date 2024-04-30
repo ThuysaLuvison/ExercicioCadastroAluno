34e973f2fda9723119b4b9e9e4f352598b909c46
1072410483 <1072410483@UNS11DT099858.animaedu.intranet>
2024-04-30 20:17:10 -0300

    Muitas coisas arrumadas

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import view.FrmMenuPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
+
+        objetotela.setVisible(true);
+    }
+}
